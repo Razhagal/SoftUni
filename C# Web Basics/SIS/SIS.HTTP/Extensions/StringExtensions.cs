2747e7802c4c642f4085519e9c0d6c4b063179d7
h.petrov <hpetrovv@gmail.com>
2025-07-24 21:18:38 +0300

    Added string extensions class

---
+++ C# Web Basics/SIS/SIS.HTTP/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SIS.HTTP.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Capitalize(this string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+            else if (input == "")
+            {
+                throw new ArgumentException($"{nameof(input)} cannot be empty.", nameof(input));
+            }
+
+            return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+        }
+    }
+}
