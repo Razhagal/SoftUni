99f6196b79104e8043641c596d640de13285e0de
h.petrov <hpetrovv@gmail.com>
2025-07-25 00:10:19 +0300

    Added Http header class and headers holding collection

---
+++ C# Web Basics/SIS/SIS.HTTP/Headers/HttpHeader.cs
@@ -0,0 +1,26 @@
+﻿
+using SIS.HTTP.Common;
+
+namespace SIS.HTTP.Headers
+{
+    public class HttpHeader
+    {
+        public HttpHeader(string key, string value)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+            CoreValidator.ThrowIfNullOrEmpty(value, nameof(value));
+
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public string Key { get; } = null!;
+
+        public string Value { get; } = null!;
+
+        public override string ToString()
+        {
+            return $"{this.Key}: {this.Value}";
+        }
+    }
+}
---
+++ C# Web Basics/SIS/SIS.HTTP/Headers/HttpHeaderCollection.cs
@@ -0,0 +1,54 @@
+﻿using SIS.HTTP.Common;
+using System.Text;
+
+namespace SIS.HTTP.Headers
+{
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private readonly Dictionary<string, HttpHeader> headers;
+
+        public HttpHeaderCollection()
+        {
+            this.headers = new Dictionary<string, HttpHeader>();
+        }
+
+        public void AddHeader(HttpHeader header)
+        {
+            if (header != null)
+            {
+                this.headers[header.Key] = header;
+            }
+        }
+
+        public bool ContainsHeader(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return false;
+            }
+
+            return this.headers.ContainsKey(key);
+        }
+
+        public HttpHeader? GetHeader(string key)
+        {
+            if (!this.ContainsHeader(key))
+            {
+                return null;
+            }
+            
+            return this.headers[key];
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var header in this.headers)
+            {
+                sb.AppendLine($"{header.Value.ToString()}{GlobalConstants.HttpNewLine}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
