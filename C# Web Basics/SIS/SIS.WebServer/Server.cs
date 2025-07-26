190801ff294b3d97faf45d83e9a4fca07406fa9d
h.petrov <hpetrovv@gmail.com>
2025-07-26 20:05:58 +0300

    Added Server and ConnectionHandler classes for working with the http requests

---
+++ C# Web Basics/SIS/SIS.WebServer/Server.cs
@@ -0,0 +1,50 @@
+﻿using SIS.HTTP.Common;
+using SIS.WebServer.Routing;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace SIS.WebServer
+{
+    public class Server
+    {
+        private const string LocalhostIpAddress = "127.0.0.1";
+
+        private readonly int port;
+        private readonly TcpListener listener;
+        private readonly IServerRoutingTable serverRoutingTable;
+
+        private bool isRunning;
+
+        public Server(int port, IServerRoutingTable serverRoutingTable)
+        {
+            CoreValidator.ThrowIfNull(serverRoutingTable, nameof(serverRoutingTable));
+
+            this.port = port;
+            this.listener = new TcpListener(IPAddress.Parse(LocalhostIpAddress), port);
+            this.serverRoutingTable = serverRoutingTable;
+        }
+
+        public void Run()
+        {
+            this.listener.Start();
+            this.isRunning = true;
+
+            Console.WriteLine($"Server started at http://{LocalhostIpAddress}:{this.port}");
+
+            while (this.isRunning)
+            {
+                Console.WriteLine("Waiting for client...");
+
+                var client = this.listener.AcceptSocket();
+                Task.Run(() => this.ListenAsync(client));
+            }
+        }
+
+        public async Task ListenAsync(Socket client)
+        {
+            ConnectionHandler connectionHandler = new ConnectionHandler(client, this.serverRoutingTable);
+            await connectionHandler.ProcessRequestAsync();
+        }
+    }
+}
