c0ba7e1088d45b059e0198abb3d19c8de9af3d5f
Razhagal <hpetrovv@gmail.com>
2025-08-11 00:02:47 +0300

    Added the rest of the required controllers for the exam preparation app

---
+++ C# Web Basics/SIS/Apps/IRunes/IRunes.App/Controllers/TracksController.cs
@@ -0,0 +1,80 @@
+﻿using IRunes.App.ViewModels.Tracks;
+using IRunes.Models;
+using IRunes.Services;
+
+using SIS.HTTP.Responses;
+
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes.Http;
+using SIS.MvcFramework.Attributes.Security;
+
+namespace IRunes.App.Controllers
+{
+    public class TracksController : Controller
+    {
+        private readonly ITrackService trackService;
+        private readonly IAlbumService albumService;
+
+        public TracksController(ITrackService trackService, IAlbumService albumService)
+        {
+            this.trackService = trackService;
+            this.albumService = albumService;
+        }
+
+        [Authorize]
+        public HttpResponse Create(string albumId)
+        {
+            return this.View(new TrackCreateViewModel { AlbumId = albumId });
+        }
+
+        [Authorize]
+        [HttpPost]
+        public HttpResponse Create(TrackCreateInputModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Redirect("/");
+            }
+
+            Track trackFromDb = new Track()
+            {
+                Name = model.Name,
+                Link = model.Link,
+                Price = model.Price,
+                AlbumId = model.AlbumId
+            };
+
+            if (!this.albumService.AddTrackToAlbum(model.AlbumId, trackFromDb))
+            {
+                return this.Redirect("/Albums/All");
+            }
+
+            return this.Redirect($"/Albums/Details?id={model.AlbumId}");
+        }
+
+        [Authorize]
+        public HttpResponse Details(TrackDetailsInputModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Redirect($"/Albums/All");
+            }
+
+            Track trackFromDb = this.trackService.GetTrackById(model.TrackId);
+            if (trackFromDb == null)
+            {
+                return this.Redirect($"/Albums/Details?id={model.AlbumId}");
+            }
+
+            TrackDetailsViewModel trackDetailsViewModel = new TrackDetailsViewModel()
+            {
+                Name = trackFromDb.Name,
+                Price = trackFromDb.Price.ToString(),
+                Link = trackFromDb.Link,
+                AlbumId = trackFromDb.AlbumId
+            };
+
+            return this.View(trackDetailsViewModel);
+        }
+    }
+}
---
+++ C# Web Basics/SIS/Apps/IRunes/IRunes.App/Controllers/UserController.cs
@@ -0,0 +1,88 @@
+﻿using IRunes.App.ViewModels.Users;
+using IRunes.Models;
+using IRunes.Services;
+
+using SIS.HTTP.Responses;
+
+using SIS.MvcFramework;
+using SIS.MvcFramework.Attributes.Action;
+using SIS.MvcFramework.Attributes.Http;
+
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IRunes.App.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public HttpResponse Login()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Login(UserLoginInputModel model)
+        {
+            User userFromDb = this.userService.GetUserByUsernameAndPassword(model.Username, this.HashPassword(model.Password));
+
+            if (userFromDb == null)
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            this.SignIn(userFromDb.Id, userFromDb.Username, userFromDb.Email);
+            return this.Redirect("/");
+        }
+
+        public HttpResponse Register()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public HttpResponse Register(UserRegisterInputModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            if (model.Password != model.ConfirmPassword)
+            {
+                return this.Redirect("/Users/Register");
+            }
+
+            User user = new User()
+            {
+                Username = model.Username,
+                Password = this.HashPassword(model.Password),
+                Email = model.Email
+            };
+
+            this.userService.CreateUser(user);
+            return this.Redirect("/Users/Login");
+        }
+
+        public HttpResponse Logout()
+        {
+            this.SignOut();
+            return this.Redirect("/");
+        }
+
+        [NonAction]
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                return Encoding.UTF8.GetString(sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password)));
+            }
+        }
+    }
+}
